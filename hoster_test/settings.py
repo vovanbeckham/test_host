c78667f366cce65804e789302cf7968b3153dbd6
Beckham <vovanbeckham@yandex.ru>
2025-02-24 20:58:27 +0300

    add mysql

--- hoster_test/settings.py
+++ hoster_test/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hoster_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatahubs_test_data',
+        'USER': 'DB_USER',
+        'PASSWORD': '56LzLLqoJdk+',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
